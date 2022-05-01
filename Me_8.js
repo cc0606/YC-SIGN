7621922c65f17117d69e9722ec6a6796970ada64
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-01 15:07:34 +0800

    Update Me_8.js

--- Me_8.js
+++ Me_8.js
@@ -28,6 +28,7 @@ const myRequest = {
 
 $task.fetch(myRequest).then(response => {
     console.log(response.statusCode + "\n\n" + response.body);
+    $notify("元初15397470261", "Subtitle", response.body); // Success!
     $done();
 }, reason => {
     console.log(reason.error);
