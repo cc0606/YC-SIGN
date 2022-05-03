bd212f53b8713c0c42bcbe02a326625e6aca000b
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-03 11:19:18 +0800

    Update Me_9.js

--- Me_9.js
+++ Me_9.js
@@ -28,7 +28,7 @@ const myRequest = {
 
 $task.fetch(myRequest).then(response => {
     console.log(response.statusCode + "\n\n" + response.body);
-    $notify("元初13295867082", "Subtitle", response.body); // Success!
+    $notify("W9", "元初13295867082", response.body); // Success!
     $done();
 }, reason => {
     console.log(reason.error);
