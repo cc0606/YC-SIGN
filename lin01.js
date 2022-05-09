28c76132ffe56df53a6ef80547ae4ce687776b2b
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-09 22:55:13 +0800

    Update lin01.js

--- lin01.js
+++ lin01.js
@@ -7,7 +7,7 @@
 const url = `https://shop97476437.youzan.com/wscump/checkin/checkinV2.json?checkinId=1999740&kdt_id=97284269`;
 const method = `GET`;
 const headers = {
-'Cookie' : `trace_sdk_context_pv_id=/wscump/checkin/result~1f28ebc7-53a4-4b56-9cae-4e980ba31d4c; yz_log_seqb=1651422230617; yz_log_seqn=58; dfp=e1b7f1d699d664786335ca23d2110931; rdfp=e1b7f1d699d664786335ca23d2110931; _kdt_id_=97284269; KDTSESSIONID=YZ970480703028031488YZPW3E4oXO; nobody_sign=YZ970480703028031488YZPW3E4oXO; captcha_sid=YZ970481435076677632YZk5sCfXB6; loc_dfp=47d6db0ded76d75e82f993b41adb1658; yz_log_ftime=1651422230524; yz_log_uuid=f07c91de-7ea3-5fdb-98d6-d589f74a74f3`,
+'Cookie' : `trace_sdk_context_pv_id=/wscuser/membercenter~256133ca-f79a-4724-805f-02979fa5c919; _efmdata=bZ%2FH6A%2BSEbaDBywKq2LFsNnxaNC%2Fn%2FwP2ikfjmBhxTQ127nMtArfO1FzcRnCzZIG5ymcqS0%2FcwbaQzMr2QRL9vJo3MdiCmtEENus4lzt0Yg%3D; _exid=My7Gicr1V3q%2B1strX7zCZ1GvjIQrLFcVYX%2FFvSkhvQ4v6e1vN9gQ9Og8eEITZYdtG4hBoCyvxo6KhNiM27UtnQ%3D%3D; ec=IKVYvbR8-1652106687743-1b346c50f527498159625; yz_fp_hash=ecc62ce86ce8ebf96e837d1ae15fc174; yz_log_seqb=1652106686313; yz_log_seqn=12; _kdt_id_=97284269; yz_ep_page_track=9VzfSWEgFy%2BWzoi7n6Zm0g%3D%3D; yz_ep_page_type_track=iDJ3GNJDHbhHtOl6W3j3ZA%3D%3D; dfp=44b29738b5ad7504b639891e8a298c01962ae21f; yz_log_ftime=1652106686245; yz_log_uuid=3bc0bcff-b1dd-8c51-fbc7-af19c5ce91b8; KDTSESSIONID=YZ972436750357721088YZTm4gAMdS; nobody_sign=YZ972436750357721088YZTm4gAMdS; kdt_id=99209606; `,
 'Accept' : `application/json, text/plain, */*`,
 'Connection' : `keep-alive`,
 'Referer' : `https://shop97476437.youzan.com/wscump/checkin/result?kdt_id=97284269`,
