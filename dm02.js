456aa2dc39218d6b10d10e7328ef7e401dd3f429
cc0606 <74646647+cc0606@users.noreply.github.com>
2022-05-14 20:58:28 +0800

    Update dm02.js

--- dm02.js
+++ dm02.js
@@ -7,7 +7,7 @@
 const url = `https://shop97476437.youzan.com/wscump/checkin/checkinV2.json?checkinId=1999740&kdt_id=97284269`;
 const method = `GET`;
 const headers = {
-'Cookie' : `trace_sdk_context_pv_id=/wscuser/membercenter~77e6a255-4dd6-412a-abb5-9dd0ad37f53a; yz_log_seqb=1651422396087; yz_log_seqn=136; _kdt_id_=97284269; KDTSESSIONID=YZ970481397601148928YZYGBLsCld; nobody_sign=YZ970481397601148928YZYGBLsCld; captcha_sid=YZ970481443954421760YZPCZx3nhT; dfp=0025e603b45175c2eaec5765d2195521; loc_dfp=b6d6a23e3e2b48613298839dae2ccbfc; yz_log_ftime=1651422396023; yz_log_uuid=6fe5d00f-e9ad-1233-3b7e-1a90f3139f7f`,
+'Cookie' : `trace_sdk_context_pv_id=/wscuser/membercenter~5b960a8e-4955-491b-a334-03ad86ae31db; yz_log_seqb=1652518578576; yz_log_seqn=45; KDTSESSIONID=YZ975079120410644480YZK29KorLO; _kdt_id_=97284269; nobody_sign=YZ975079120410644480YZK29KorLO; captcha_sid=YZ975079408727969792YZeaTzMjlM; dfp=a5f62ddcce4c185df673113c7cf3a043; loc_dfp=29b73d1138b32a922ae1e00f7a573829; yz_log_ftime=1652518578516; yz_log_uuid=c8a4cac4-b133-ddbe-3f2a-3b11af22f141`,
 'Accept' : `application/json, text/plain, */*`,
 'Connection' : `keep-alive`,
 'Referer' : `https://shop97476437.youzan.com/wscump/checkin/result?kdt_id=97284269`,
